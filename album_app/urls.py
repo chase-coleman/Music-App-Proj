39212979f1976818d50d375a1551b9c6af9397e1
YOUR_NAME <YOUR_EMAIL>
2025-04-02 16:17:27 -0500

    Finished creating views for each app to view all instances of that app

--- album_app/urls.py
+++ album_app/urls.py
@@ -1,2 +1,6 @@
 from django.urls import path
-from .views import All_
+from .views import All_albums
+
+urlpatterns = [
+  path('all/', All_albums.as_view(), name="all_albums"),
+]
--- song_app/views.py
+++ song_app/views.py
@@ -1,3 +1,11 @@
+from .serializers import Song, SongSerializer
+from rest_framework.views import Response, APIView # importing needed Response and APIView classes
+from django.http import JsonResponse
 from django.shortcuts import render
+from rest_framework import status as s
 
 # Create your views here.
+class All_songs(APIView):
+  def get(self, request):
+    all_songs = SongSerializer(Song.objects.all(), many=True)
+    return Response(all_songs.data, status=s.HTTP_200_OK)
