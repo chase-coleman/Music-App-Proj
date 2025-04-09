673b12392e16668a74745b1070739d8df0e1c136
YOUR_NAME <YOUR_EMAIL>
2025-04-09 09:54:22 -0500

    created dev branch. added navbar component.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Nav } from "react-bootstrap";
+import Navbar from "../components/Navbar";
 
 const login = () => {
 
@@ -9,6 +10,7 @@ const login = () => {
 function HomePage() {
   return (
     <>
+    <Navbar />
     <h1>Home Page</h1>
     <Nav.Link as={Link} to="/login">
     Login
