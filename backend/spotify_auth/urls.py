4b91e8510199ba5633c5da6966a3bd4b24039f8e
YOUR_NAME <YOUR_EMAIL>
2025-04-10 14:45:50 -0500

    did some refactoring on the names of functions and variables

--- backend/spotify_auth/urls.py
+++ backend/spotify_auth/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import Spotify_Callback_View
 
 urlpatterns = [
-  path('', Spotify_Callback_View.as_view(), name="spotify_callback")
+  path('', Spotify_Callback_View.as_view(), name="spotify_callback"),
+  path('<str:item>', Spotify_Callback_View.as_view(), name="spotify_callback")
 ]
--- backend/spotify_auth/views.py
+++ backend/spotify_auth/views.py
@@ -19,14 +19,17 @@ import json
 # ASCII is standardized characters based off the english alphabet
 # ASCII : aA-zZ, 0-9, symbols(!@#$...), special characters(newline, tab, backspace...), control characters
 
-# TO DO : make it so we can store the spotify access token - maybe make a timer for it to generate a new token
-#         every hour?
+# TO DO : refactor code so that I get a refresh token instead of an access token
+#         so that the app doesn't have to make an access token request to Spotify every time the endpoint is called
+# TO DO : create methods that parse through the return data based on if it is a song, artist, etc
+# TO DO : create models + model.fields to store a user's liked artists, songs, etc
+# TO DO : using this views.py file, create model instances for when a user wants to like/follow that stuff
 
 load_dotenv() # we have to load the environment variables from .env
 
 SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID") # my personal client ID for Spotify
 SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET") # my Spotify API client secret number
-spotify_item_search_url = "https://api.spotify.com/v1/"
+spotify_item_search_url = "https://api.spotify.com/v1/" # endpoint for Spotify's item search URL
 
 
 # this function will call Spotify's API and get an access-token for our app
@@ -48,35 +51,40 @@ def get_spotify_token():
   token = json_result['access_token']
   return token
 
-spotify_token = get_spotify_token()
-
 def get_auth_header(spotify_token):
   return {"Authorization": "Bearer " + spotify_token}
 
-headers = get_auth_header(spotify_token)
-
 # STEPS TO GET item:
 # search for item by name
-# query = f"q={artist_name}&type=artist&limit=1" returns only 1 answer
-def search_for_artist(spotify_token, artist_name):
-  query = f"?q={artist_name}&type=artist&limit=1" # comma delimited list
+# type=... is a list of types that the API will return with a matching searched item (ie artists, albums, tracks(songs)
+# limit=... is limiting how many items are being returned by the API (1 = 1 item returned, 3 = 3 items returned, etc)
+def search_for_item(spotify_token, item, headers):
+  query = f"?q={item}&type=artist,album,track&limit=10" # comma delimited list
 
   # this query is calling an endpoint that searches Spotify's db using a string
   # have to do this first because we need to get the artist/genre/album/track id to store in our database
   query_url = spotify_item_search_url + "search" + query
-  result = get(query_url, headers=headers)
 
-  json_result = json.loads(result.content)
+  # make a GET request to the Spotify endpoint that searches for a match in the specified types (inside of the query var)
+  search_result = get(query_url, headers=headers)
+   
+   # converting the search results to a dictionary
+  json_result = json.loads(search_result.content)
   return json_result
 
 
 # Create your views here.
 class Spotify_Callback_View(TokenReq):  
-  def get(self, request):
+  def get(self, request, item=None):
     # get the user's authorization token
     user_token = Token.objects.get(user=request.user)
-    answer = search_for_artist(spotify_token, artist_name="Blood Orange")
-    return Response(answer['artists']['items'][0]['id'])
+    spotify_token = get_spotify_token()
+    headers = get_auth_header(spotify_token)
+    if item:
+      answer = search_for_item(spotify_token, item=item, headers=headers)
+      # ['artists']['items'][0]['id']
+      return Response(answer)
+    return Response(True)
   
 
 
