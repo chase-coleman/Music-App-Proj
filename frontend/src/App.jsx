1a9805efff7145ffea172345a44a4aac16d40e22
YOUR_NAME <YOUR_EMAIL>
2025-04-16 11:52:53 -0500

    after a long, and frustrating day & a half, I have finally gotten a refresh token from spotify to use on my personal account for this app.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,22 @@
 import { Outlet } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import './App.css'
+import axios from './axios'
 
 
 export default function App() {
-  const [userToken, setUserToken] = useState('')
+  const [userToken, setUserToken] = useState('');
+  
   
   useEffect(() => {
-    localStorage.setItem("token", userToken)
-    console.log("setting userToken:", userToken)
-  }, [userToken])
+    if (userToken){
+      localStorage.setItem("token", userToken)
+      console.log("setting userToken:", userToken)
+      // getSpotifyRefreshToken()
+    }
+  }, [userToken]);
 
-  // const userToken = localStorage.getItem("token"); // grab user's token
-  
+
   return <Outlet context={{userToken, setUserToken}}/>
 }
 
