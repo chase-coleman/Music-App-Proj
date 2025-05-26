08bbb1e745f15786e719069a6998ab1efa47abf5
YOUR_NAME <YOUR_EMAIL>
2025-05-26 12:57:49 -0700

    Added a button to view the user's current queue. renders conditionally in the PlaylistSongs section on the homepage.

--- frontend/src/components/PlaylistSongs.jsx
+++ frontend/src/components/PlaylistSongs.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { use, useContext, useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import axios from "../axios";
 import { useOutletContext} from "react-router-dom";
@@ -8,8 +8,8 @@ import { getTracks } from "../utils/MusicUtils";
 import { HomePageContext } from "../pages/HomePage";
 
 const PlaylistSongs = () => {
-  // const [playlistTracks, setPlaylistTracks] = useState([]);
-  const { musicActive, setMusicActive, setCurrentTrack } = useOutletContext()
+  const [queueView, setQueueView] = useState(false)
+  const { musicActive, setMusicActive, setCurrentTrack, queue } = useOutletContext()
 
   const { playlistView, playlistTracks, setPlaylistTracks, removeTrack } = useContext(HomePageContext)
 
@@ -18,13 +18,22 @@ const PlaylistSongs = () => {
     getTracks(playlistView.name, setPlaylistTracks)
   }, []);
 
+  const viewQueue = () => {
+    if (!queueView) {
+      setQueueView(true)
+    } else setQueueView(false)
+  }
+  useEffect(() => {
+    console.log(queueView)
+  }, [queueView])
 
   return (
     <>
       {playlistView ? (
         <div className="song-container w-[100%] h-[100%] flex flex-col items-center">
         <ul className="list bg-base-100 rounded-box shadow-md w-[100%] p-0">
-            <div className="flex flex-col">
+            <div className="flex flex-row justify-between">
+              <button className="queue-btn p-1" onClick={viewQueue}>Queue</button>
             <span className="text-center">
             {playlistView.name}
             </span>
@@ -32,7 +41,14 @@ const PlaylistSongs = () => {
               {playlistView.description}
             </span>
             </div>
-          {playlistTracks.map((track) => (
+          {queueView ? 
+          queue.map((track) => (
+            <li className="list-row p-1 gap-1" key={track.id}>
+              <PlaylistTracks
+              track={track}/>
+            </li>
+          )) : 
+          playlistTracks.map((track) => (
             <li className="list-row p-1 gap-1" key={track.id}>
               <PlaylistTracks
                 track={track}
